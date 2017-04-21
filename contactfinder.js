268e0f1e7639ea7e0c8c027390d6bd7ce66fa308
Cesar <cesaroach@gmail.com>
2017-04-21 17:26:38 -0300

    Cheerio installed, code working

--- contactfinder.js
+++ contactfinder.js
@@ -1,10 +1,21 @@
 const request = require('superagent');
-let URL = process.argv[];
+const cheerio = require('cheerio')
+let URL = process.argv[2];
 
 function checkContact(val) {
-  request
+    request
     .get(val)
     .end(function(err, res) {
+        if (err !== null && err.response === undefined) {
+          console.log('Wrong URL');
+          return;
+        };
+      const $ = cheerio.load(res.text);
+        if ($("a[href*='contact']").length !== 0) {
+          console.log($("a[href*='contact']")[0].attribs.href);
+        } else {
+          console.log("There is no contact link");
+        };
     });
 };
 checkContact(URL);
